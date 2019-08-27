6a90839dcfac4fcea8622a8045b7d52800e757f0
rzzldzzldesign <letsrazzledazzle@gmail.com>
2019-08-27 17:48:50 +0200

    Major overhauls, added stickers to hero and updated about section.

--- js/darkmode.js
+++ js/darkmode.js
@@ -5,18 +5,18 @@ let darkmode = true;
 
 toggle.addEventListener("click", e => {
     if (darkmode) {
-        console.log("CLICKED!");
-        root.style.setProperty("--color-main", "#1c1c1c");
+        root.style.setProperty("--color-main", "blue");
         root.style.setProperty("--color-bg", "whitesmoke");
         root.style.setProperty("--blend-mode", "screen");
+        root.style.setProperty("--color-overlay", "blue");
 
         //root.style.setProperty("--color-bg-trns", "rgba(245,245,245,0)");
         darkmode = false;
     } else {
-        console.log("CLICKED2!");
         root.style.setProperty("--color-main", "aquamarine");
         root.style.setProperty("--color-bg", "#1c1c1c");
         root.style.setProperty("--blend-mode", "multiply");
+        root.style.setProperty("--color-overlay", "aquamarine");
         darkmode = true;
     }
 });
---
+++ js/stickerz.js
@@ -0,0 +1,48 @@
+const imgs = [
+    "../img/stickerz/rzzl-stckr-global-noise.gif",
+    "../img/stickerz/rzzl-stckr-grow.gif",
+    "../img/stickerz/rzzl-stckr-magickeye.gif",
+    "../img/stickerz/rzzl-stckr-penta.gif",
+    "../img/stickerz/rzzl-stckr-rxdbmap.png",
+    "../img/stickerz/rzzl-stckr-skelly.png",
+    "../img/stickerz/rzzl-stckr-thanks.png",
+    "../img/stickerz/rzzl-stckr-badge.gif",
+    "../img/stickerz/rzzl-stckr-grunge.png",
+    "../img/stickerz/rzzl-stckr-navy.gif",
+    "../img/stickerz/rzzl-stckr-flamez.gif"
+];
+
+let i = 0;
+
+function placeSticker(x, y) {
+    const nextImage = imgs[i];
+
+    const img = document.createElement("img");
+    img.setAttribute("src", nextImage);
+    img.style.left = x + "px";
+    img.style.top = y + "px";
+    img.style.transform =
+        "translate(-50%, -50%) scale(0.4) rotate(" +
+        (Math.random() * 20 - 10) +
+        "deg)";
+
+    document.querySelector(".welcome-section").appendChild(img);
+    i++;
+    if (i >= imgs.length) {
+        i = 0;
+    }
+}
+
+document
+    .querySelector(".welcome-section")
+    .addEventListener("click", function(e) {
+        e.preventDefault();
+        placeSticker(e.pageX, e.pageY);
+    });
+
+document
+    .querySelector(".welcome-section")
+    .addEventListener("touchend", function(e) {
+        e.preventDefault();
+        placeSticker(e.pageX, e.pageY);
+    });
