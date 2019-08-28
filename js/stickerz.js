54247a97fd55f6133706a6fcf8ac613c92768a62
rzzldzzldesign <letsrazzledazzle@gmail.com>
2019-08-28 19:55:18 +0200

    Update stickers, removed jQuery and scroll.js

--- js/stickerz.js
+++ js/stickerz.js
@@ -1,8 +1,8 @@
 const imgs = [
     "../img/stickerz/rzzl-stckr-global-noise.gif",
     "../img/stickerz/rzzl-stckr-grow.gif",
-    "../img/stickerz/rzzl-stckr-magickeye.gif",
     "../img/stickerz/rzzl-stckr-penta.gif",
+    "../img/stickerz/rzzl-stckr-snakegrabber.png",
     "../img/stickerz/rzzl-stckr-rxdbmap.png",
     "../img/stickerz/rzzl-stckr-skelly.png",
     "../img/stickerz/rzzl-stckr-thanks.png",
